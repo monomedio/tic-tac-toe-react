1dd54767f6f43849f0dc159d9bc230f02886e2e7
Sam Hui <samhuifromhk@gmail.com>
2023-01-05 17:27:48 -0500

    Implement game functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,82 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+function Square({ onSquareClick, value }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
+
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    if (squares[i] || winner) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O";
+    setXIsNext(!xIsNext);
+    setSquares(nextSquares);
+  };
+
+  let winner = calculateWinner(squares);
+  let status = winner
+    ? `The winner is: ${winner}!`
+    : `Next player: ${xIsNext ? "X" : "O"}`;
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square onSquareClick={() => handleClick(0)} value={squares[0]} />
+        <Square onSquareClick={() => handleClick(1)} value={squares[1]} />
+        <Square onSquareClick={() => handleClick(2)} value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square onSquareClick={() => handleClick(3)} value={squares[3]} />
+        <Square onSquareClick={() => handleClick(4)} value={squares[4]} />
+        <Square onSquareClick={() => handleClick(5)} value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square onSquareClick={() => handleClick(6)} value={squares[6]} />
+        <Square onSquareClick={() => handleClick(7)} value={squares[7]} />
+        <Square onSquareClick={() => handleClick(8)} value={squares[8]} />
+      </div>
+    </>
+  );
+}
+
+// calculates a winner (if any) based on an input array squares
+// returns an element in the set { 'X', 'O', 'nobody', null}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // check for winning combinations
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  // decide whether the game is a draw or if it should continue
+  if (!squares.includes(null)) {
+    return "nobody";
+  } else {
+    return null;
+  }
+}
+
+export default Board;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App";
+import Board from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <Board />
+  </React.StrictMode>
 );
